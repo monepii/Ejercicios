8f1c9e5ec57e3a1e1455b131ad3a9233d2c9ade8
monepii <em-ben-10@hotmail.com>
2025-04-03 10:10:40 -0700

    Add files via upload

---
+++ python8.py
@@ -0,0 +1,20 @@
+
+x = 0
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print("y =", y)
+
+x = 1
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print("y =", y)
+
+x = -1
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print("y =", y)
+
+
+val1 = int(input("Ingresa un numero: "))
+val2 = int(input("Ingresa un numero: "))
+val3 = int(input("Ingresa un numero: "))
