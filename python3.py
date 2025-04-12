b6dcca012e3005017145bfc4d7ffe5670331d1f9
monepii <em-ben-10@hotmail.com>
2025-04-12 00:41:12 -0700

    Delete python3.py

--- python3.py
+++
@@ -1,8 +0,0 @@
-entrada = input("Ingresa e nombre de una planta: ")
-
-if entrada == "Espatifilo":
-    print("Si, ¡El Espatifilo! es la mejor planta de todos los tiempos! ")
-elif entrada =="espatifilo":
-    print("No, ¡quiero un gran Espatifilo! ")
-else: 
-    print("¡Espatifilo!, ¡No "+ entrada + "! ")
