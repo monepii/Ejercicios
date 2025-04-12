342755337b781b8e2b1f0c721fa9f33ae0279302
monepii <em-ben-10@hotmail.com>
2025-04-12 00:40:29 -0700

    Delete e6.java

--- e6.java
+++
@@ -1,8 +0,0 @@
-    public static void main(String[] args) {
-        String cadena = "Hola mundo";
-        String[] output = cadena.split("");
-		for (String y : output) {
-			System.out.println(y);
-		}
-    } 
-
